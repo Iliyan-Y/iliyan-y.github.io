085657478699868d47e7cd3189fd245ceb1e048b
Iliyan-Y <maximizebg@gmail.com>
2020-02-20 00:10:10 +0000

    Css and Div added

--- QuotePage/QuotePage.js
+++ QuotePage/QuotePage.js
@@ -3,27 +3,34 @@ function back(){
   var aBack = document.createElement('a');
   var backTxt = document.createTextNode("Back");
   aBack.appendChild(backTxt);
-  aBack.setAttribute("href", "../index.html")
+  aBack.setAttribute("href", "../index.html");
+  aBack.setAttribute("id", "back");
   document.body.appendChild(aBack);
 }
 function quoteFunction() {
   var quotes = ['one','two','three'];
+  //create new div
+  var innerDiv = document.createElement('div');
+  innerDiv.setAttribute("id", "innerDiv");
+  document.body.appendChild(innerDiv);
   //create random number based on the lenght of the array
   var quotesInTotal = quotes.length;
   var randomNumber = Math.floor(Math.random() * quotesInTotal);
   //create paragraph with the quotes
   var createP = document.createElement('p');
-  createP.setAttribute("id", "p1")
+  createP.setAttribute("id", "p1");
   //get random quote
   var pTextQuote = document.createTextNode(quotes[randomNumber]);
   //add the quote to the page
   createP.appendChild(pTextQuote);
-  document.body.appendChild(createP);
+  innerDiv.appendChild(createP);
 
   function quoteButton() {
     var createButton = document.createElement('button')
-    createButton.innerHTML = "New Quote"
-    document.body.appendChild(createButton);
+    createButton.innerHTML = "New Quote";
+    createButton.setAttribute("class", "btn");
+    createButton.setAttribute("id", "btn-1");
+    innerDiv.appendChild(createButton);
 
     createButton.addEventListener("click", function() {
       var newQote = document.getElementById('p1');
@@ -38,7 +45,9 @@ function quoteFunction() {
   function backButton() {
     var createBackButton = document.createElement('button')
     createBackButton.innerHTML = "First Quote";
-    document.body.appendChild(createBackButton);
+    createBackButton.setAttribute("class", "btn");
+    createBackButton.setAttribute("id", "btn-2");
+    innerDiv.appendChild(createBackButton);
 
     createBackButton.addEventListener("click", function() {
       createP.innerHTML = "";
@@ -49,7 +58,6 @@ function quoteFunction() {
  backButton();
  quoteButton();
 }
-
 
 back();
 quoteFunction();
